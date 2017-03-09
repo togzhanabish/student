924cf9ab19b21e6c15ce006625d1b1f61ba42d44
Togzhan <togzhanabish369@gmail.com>
2017-03-09 12:15:57 +0600

    student

---
+++ student/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    class Program
+    {
+        class Student
+        {
+            private string fname;
+            private string lname;
+            private double gpa;
+
+            public Student() { }
+
+            public Student(string fname, string lname, double gpa)
+            {
+                this.fname = fname;
+                this.lname = lname;
+                this.gpa = gpa;
+            }
+
+            public override string ToString()
+            {
+                return fname + " " + lname + " " + gpa;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Student s = new Student("Togzhan", "Abish", 3.8);
+            Console.Write(s.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
